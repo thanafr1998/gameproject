5b7a21961fe7d3f40a29661fee499a71a3961d30
thanafr1998 <thanafr1998@Hotmail.com>
2018-12-06 21:42:58 +0700

    sound

---
+++ src/model/Sound.java
@@ -0,0 +1,11 @@
+package model;
+
+import javafx.scene.media.AudioClip;
+
+public class Sound {
+
+	public static AudioClip enterSound = new AudioClip(ClassLoader.getSystemResource("model/resources/button5.wav").toString());
+	
+
+	
+}
